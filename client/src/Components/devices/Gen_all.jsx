311a7b64b6c18bf0d4ae6ac0bc3ba7a4acb8d957
YashashavGoyal <76003258+YashashavGoyal@users.noreply.github.com>
2024-10-05 08:11:23 +0530

    ..

--- client/src/Components/devices/Gen_all.jsx
+++ client/src/Components/devices/Gen_all.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./Gen_all.css"
-import GenSelector from './Genselector/Genselector';
+import GenSelector from './Genselector/GenSelector';
 function Gen_all() {
   return (
     <>
